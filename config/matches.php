51fab0bc332ba3445b7d1740c468c65d112014c6
avenirer <avenir.ro@gmail.com>
2014-08-08 15:22:46 +0300

    Create matches.php
    Create separate file for configuration of Matches. First commit

---
+++ config/matches.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Configuration for Matches controller
+ */
+
+ 
+/**
+ * Configure the templates directory
+ */ 
+$config['templates'] = 'application/views/matches_templates/';
+
+
+/**
+ * Tell the Matches what controller to extend
+ */
+$config['c_extends'] = 'CI';
+
+/**
+ * Tell the Matches what model to extend
+ */
+$config['mo_extends'] = 'CI';
+
+/**
+ * Tell the Matches what Migration to extend
+ */
+$config['mi_extends'] = 'CI';
