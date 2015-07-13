f86cc74c2160b8abfad841c4f290b07804886f20
Adrian Voicu <avenir.ro@gmail.com>
2015-07-13 11:08:46 +0300

    add und_migration() and reset_migration()

--- controllers/Matches.php
+++ controllers/Matches.php
@@ -389,6 +389,58 @@ class Matches extends CI_Controller {
         {
             echo $this->_ret.'The migration has concluded successfully.';
         }
+        return TRUE;
+    }
+
+    public function undo_migration($version = NULL)
+    {
+        $this->load->library('migration');
+        $migrations = $this->migration->find_migrations();
+        $migration_keys = array();
+        foreach($migrations as $key => $migration)
+        {
+            $migration_keys[] = $key;
+        }
+        if(isset($version) && array_key_exists($version,$migrations) && $this->migration->version($version))
+        {
+            echo $this->_ret.'The migration was reset to the version: '.$version;
+            return TRUE;
+        }
+        elseif(isset($version) && !array_key_exists($version,$migrations))
+        {
+            echo $this->_ret.'The migration with version number '.$version.' doesn\'t exist.';
+        }
+        else
+        {
+            $penultimate = (sizeof($migration_keys)==1) ? 0 : $migration_keys[sizeof($migration_keys) - 2];
+
+            if($this->migration->version($penultimate))
+            {
+                echo $this->_ret.'The migration has been rolled back successfully.';
+                return TRUE;
+            }
+            else
+            {
+                echo $this->_ret.'Couldn\'t roll back the migration.';
+                return FALSE;
+            }
+        }
+    }
+
+    public function reset_migration()
+    {
+        $this->load->library('migration');
+        if($this->migration->current()!== FALSE)
+        {
+            echo $this->_ret.'The migration was reset to the version set in the config file.';
+            return TRUE;
+        }
+        else
+        {
+            echo $this->_ret.'Couldn\'t reset migration.';
+            show_error($this->migration->error_string());
+            return FALSE;
+        }
     }
 
     public function verify_migration_enabled()
