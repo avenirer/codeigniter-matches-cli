1e32f487d9b015133171c2d84fa0239df1793e92
mehdi lamaaffar <mehdilamaafar@gmail.com>
2017-01-22 18:05:55 +0000

    update matches config file

--- config/matches.php
+++ config/matches.php
@@ -1,27 +1,27 @@
 <?php
 
 /**
- * Configuration for Matches controller
- */
-
- 
-/**
- * Configure the templates directory
- */ 
-$config['templates'] = 'views/matches_templates/';
+* Configuration for Matches controller
+*/
 
 
 /**
- * Tell the Matches what controller to extend
- */
-$config['c_extends'] = 'CI';
+* Configure the templates directory
+*/
+$config['templates'] = 'views' . DIRECTORY_SEPARATOR . 'matches_templates' . DIRECTORY_SEPARATOR;
+
 
 /**
- * Tell the Matches what model to extend
- */
-$config['mo_extends'] = 'CI';
+* Tell the Matches what controller to extend
+*/
+$config['controller_extends'] = 'CI';
 
 /**
- * Tell the Matches what Migration to extend
- */
-$config['mi_extends'] = 'CI';
+* Tell the Matches what model to extend
+*/
+$config['model_extends'] = 'CI';
+
+/**
+* Tell the Matches what Migration to extend
+*/
+$config['migration_extends'] = 'CI';
